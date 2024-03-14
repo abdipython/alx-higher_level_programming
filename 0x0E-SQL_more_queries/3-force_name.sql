66ac28f645aaabec1ce221667472a2719ff949ae
Abdulraman Saumu <abdulrahmansaumu@gmail.com>
2024-03-14 05:34:15 -0700

    first 8 sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+D
+-- creates table force_name on your MySQL server
+-- creates tables in a database
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+--It creates the table id_not_null on your MySQL server
+-- It creates a table
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
